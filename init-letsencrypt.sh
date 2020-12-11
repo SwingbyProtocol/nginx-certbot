180f9d047d61b50dedc01e9c3ec1e0002881169c
syrohei <syrohei@gmail.com>
2020-12-11 18:39:45 +0900

    fix: Updated subdomains to support indexer ws

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -6,6 +6,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
 fi
 
 domain=$DOMAIN
+btc_indexer_domain="$DOMAIN-btc-indexer"
+btc_indexer_domain="$DOMAIN-eth-indexer"
+domains="($domain $btc_indexer_domain $eth_indexer_domain)"
+
 port=$PORT
 rsa_key_size=4096
 data_path="./data/certbot"
@@ -18,7 +22,7 @@ sed -i "s/_DOMAIN_/$domain/g" "$nginx_config_path/app.conf"
 sed -i "s/_PORT_/$port/g" "$nginx_config_path/app.conf"
 
 if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domain. Continue and replace existing certificate? (y/N) " decision
+  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
     exit
   fi
@@ -32,9 +36,9 @@ if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/
   echo
 fi
 
-echo "### Creating dummy certificate for $domain ..."
-path="/etc/letsencrypt/live/$domain"
-mkdir -p "$data_path/conf/live/$domain"
+echo "### Creating dummy certificate for $domains ..."
+path="/etc/letsencrypt/live/$domains"
+mkdir -p "$data_path/conf/live/$domains"
 docker-compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
@@ -46,20 +50,25 @@ echo "### Starting nginx ..."
 docker-compose up --force-recreate -d nginx
 echo
 
-echo "### Deleting dummy certificate for $domain ..."
+echo "### Deleting dummy certificate for $domains ..."
 docker-compose run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/$domain && \
-  rm -Rf /etc/letsencrypt/archive/$domain && \
-  rm -Rf /etc/letsencrypt/renewal/$domain.conf" certbot
+  rm -Rf /etc/letsencrypt/live/$domains && \
+  rm -Rf /etc/letsencrypt/archive/$domains && \
+  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
 echo
 
-echo "### Requesting Let's Encrypt certificate for $domain ..."
+echo "### Requesting Let's Encrypt certificate for $domains ..."
 
 # Select appropriate email arg
 case "$email" in
 "") email_arg="--register-unsafely-without-email" ;;
 *) email_arg="--email $email" ;;
 esac
+
+domain_args=""
+for domain in "${domains[@]}"; do
+  domain_args="$domain_args -d $domain"
+done
 
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
@@ -68,7 +77,7 @@ docker-compose run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
-    -d $domain \
+    $domain_args \
     --rsa-key-size $rsa_key_size \
     --agree-tos \
     --force-renewal" certbot
