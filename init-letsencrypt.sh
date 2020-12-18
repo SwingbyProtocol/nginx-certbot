13398ab7492af3604735b179b8980ae0e02b44b5
syrohei <syrohei@gmail.com>
2020-12-19 04:02:58 +0900

    feat: changed checker dir path

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -40,7 +40,7 @@ if [ $withIndexer == 'true' ]; then
   sed -i "s/_WSPORT_/9131/g" "$nginx_mount_path/$eth_indexer.conf"
 fi
 
-if [ -d "$certbot_mount_path/conf/live/$domain/privkey.pem" ]; then
+if [ "$certbot_mount_path/conf/live/$domain/cert.pem" ]; then
   echo "Existing data found for $domains. Process is skipped..."
   exit 0
 fi
